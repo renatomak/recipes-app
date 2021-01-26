368fc35870071dea624037686b53bc48b240ff93
Vitor Rodrigues <vitor_rc@outlook.com>
2021-01-26 17:24:36 -0300

    Estrutura basica

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from 'react';
+import { Provider } from './context/Provider';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
