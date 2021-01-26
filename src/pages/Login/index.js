cc712c0e0afac916ae44b60833676177e274d0a6
renatomak <renato.mark.silva@gmail.com>
2021-01-26 19:09:07 -0300

    Requisito 2, 3 e 4

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChangeEmail = ({ target: { value } }) => setEmail(value);
+
+  const handleChangePassword = ({ target: { value } }) => setPassword(value);
+
   return (
     <div>
       <form>
         <input
           type="text"
           name="email"
+          value={ email }
           data-testid="email-input"
           placeholder="user@trybe.com"
+          onChange={ (e) => handleChangeEmail(e) }
         />
         <input
           type="text"
-          name="password"
+          name={ password }
           data-testid="password-input"
           placeholder="password"
+          onChange={ (e) => handleChangePassword(e) }
         />
-        <button type="button" data-testid="login-submit-btn">Entrar</button>
+        <button
+          type="button"
+          data-testid="login-submit-btn"
+
+        >
+          Entrar
+        </button>
       </form>
     </div>
   );
