5b09468b756a392c76f7dd7a34c5194f84d3d543
renatomak <renato.mark.silva@gmail.com>
2021-01-26 17:47:58 -0300

    Requisito 2 - inputs e button

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,21 @@ import React from 'react';
 function Login() {
   return (
     <div>
-      Login page
+      <form>
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          placeholder="user@trybe.com"
+        />
+        <input
+          type="text"
+          name="password"
+          data-testid="password-input"
+          placeholder="password"
+        />
+        <button type="button" data-testid="login-submit-btn">Entrar</button>
+      </form>
     </div>
   );
 }
