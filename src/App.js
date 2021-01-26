e13bfc1f3ea579a582971e1400dd20060b318388
samantabelow <samanta.below@gmail.com>
2021-01-26 19:10:09 -0300

    Lint

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from './context/Provider';
 import './App.css';
-import TelaPrincipalComidas from './pages/TelaPrincipal/index';
+import TelaPrincipalComidas from "./pages/TelaPrincipal/index";
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
         <Switch>
-          <Route to="/comidas" component={ TelaPrincipalComidas } />
+          <Route to="/comidas" component={TelaPrincipalComidas} />
         </Switch>
       </BrowserRouter>
     </Provider>
