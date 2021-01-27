ddaccb643548b9c117d8156be20882c7e48604f2
samantabelow <samanta.below@gmail.com>
2021-01-27 16:54:48 -0300

    Requisitos 11, 12 e 13

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 function Header(props) {
   const { headerText, showSearchButton } = props;
+  const [searchBar, setSearchBar] = useState(false);
   return (
-    <header>
+    <div>
       <a
         type="button"
         data-testid="profile-top-btn"
@@ -21,11 +22,24 @@ function Header(props) {
           type="button"
           data-testid="search-top-btn"
           src={ searchIcon }
+          onClick={ () => setSearchBar(!searchBar) }
         >
           <img src={ searchIcon } alt="Search icon" />
         </button>
       )}
-    </header>
+      {searchBar && (
+        <div>
+          <input type="text" data-testid="search-input"></input>
+          <label htmlFor="ingredient">Ingrediente</label>
+          <input type="radio" data-testid="ingredient-search-radio" id="ingredient" placeholder="Ingrediente"></input>
+          <label htmlFor="name">Nome</label>
+          <input type="radio" data-testid="name-search-radio" id="name" placeholder="Nome"></input>
+          <label htmlFor="first-letter">Nome</label>
+          <input type="radio" data-testid="first-letter-search-radio" id="first-letter" placeholder="Primeira letra"></input>
+          <button type="button" data-testid="exec-search-btn">Buscar</button>
+        </div>
+      )}
+    </div>
   );
 }
 
