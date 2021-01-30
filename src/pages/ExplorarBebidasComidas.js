da585c16cf9da7ea3547562cfe0db04805ebe762
renatomak <renato.mark.silva@gmail.com>
2021-01-30 17:47:48 -0300

    Requisitos 70 e 71 -  com erros

--- src/pages/ExplorarBebidasComidas.js
+++ src/pages/ExplorarBebidasComidas.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import PropTypes from 'prop-types';
+import './Explorar.css';
 import Header from './components/Header';
 
 function ExplorarBebidasComidas(props) {
-  const { recipeType } = props;
+  const { recipeType, history } = props;
   return (
     <div>
       <Header headerText={ `Explorar ${recipeType}` } showSearchButton="false" />
+
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+
+      <button
+        type="button"
+        className={ `button-${recipeType}` }
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+
+      <button
+        type="button"
+        data-testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
     </div>
   );
 }
@@ -15,4 +41,5 @@ export default ExplorarBebidasComidas;
 
 ExplorarBebidasComidas.propTypes = {
   recipeType: PropTypes.string.isRequired,
+  history: ReactRouterPropTypes.history.isRequired,
 };
