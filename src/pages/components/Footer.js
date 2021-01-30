6f1a1e5859c774c9cdba6bfb652c4613ea294fdb
renatomak <renato.mark.silva@gmail.com>
2021-01-30 13:16:02 -0300

    Requisito 20

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Footer.css';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer data-testid="footer" className="footer">
       <img src={ drinkIcon } data-testid="drinks-bottom-btn" alt="drinks" />
       <img src={ exploreIcon } data-testid="explore-bottom-btn" alt="Explore" />
       <img src={ mealIcon } data-testid="food-bottom-btn" alt="Food" />
