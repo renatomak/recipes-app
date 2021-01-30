a6b95edee40cd2bfdeb7e724ab7ee46a8b9eb13b
renatomak <renato.mark.silva@gmail.com>
2021-01-30 13:31:27 -0300

    Requisito 21

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
 
 function Explorar() {
   return (
     <div>
       <Header headerText="Explorar" showSearchButton="false" />
+      <Footer />
     </div>
   );
 }
--- src/pages/ExplorarBebidasComidas.js
+++ src/pages/ExplorarBebidasComidas.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function ExplorarBebidasComidas(props) {
   const { recipeType } = props;
   return (
     <div>
       <Header headerText={ `Explorar ${recipeType}` } showSearchButton="false" />
+      <Footer />
     </div>
   );
 }
