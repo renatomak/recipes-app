d45568773f65f91ab76113c00f3a73871c7e64fd
renatomak <renato.mark.silva@gmail.com>
2021-01-30 13:55:58 -0300

    Requisito 82 - Elementos da tela de perfil

--- cypress/integration/profile_spec.js
+++ cypress/integration/profile_spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe.skip('Profile screen', () => {
+describe('Profile screen', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/perfil', {
       onBeforeLoad(win) {
--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -5,6 +5,17 @@ function Perfil() {
   return (
     <div>
       <Header headerText="Perfil" showSearchButton="false" />
+      <div>
+        <span data-testid="profile-email"> Email </span>
+        <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button type="button" data-testid="profile-logout-btn">Sair</button>
+      </div>
     </div>
   );
 }
