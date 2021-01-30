b66561ac3b65d3bfac4edae44948e865852e8029
renatomak <renato.mark.silva@gmail.com>
2021-01-30 14:09:40 -0300

    Requisitos83 e 84 - recuperar email do local storage

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
 
+function recuperarEmail() {
+  const user = localStorage.getItem('user');
+  const email = JSON.parse(user);
+  return email;
+}
+
 function Perfil() {
+  const { email } = recuperarEmail();
+  console.log(email);
+
   return (
     <div>
       <Header headerText="Perfil" showSearchButton="false" />
       <div>
-        <span data-testid="profile-email"> Email </span>
+        <span data-testid="profile-email"> {email} </span>
         <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
         <button
           type="button"
