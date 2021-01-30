7809c9af60cd94027916e0596c86f5e9eba84390
renatomak <renato.mark.silva@gmail.com>
2021-01-30 15:06:47 -0300

    Requisitos 67 e 68 - criaçao dos botaoes de explorar comidas e bebidas

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -5,6 +5,18 @@ function Explorar() {
   return (
     <div>
       <Header headerText="Explorar" showSearchButton="false" />
+      <button
+        type="button"
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explorar Bebidas
+      </button>
     </div>
   );
 }
