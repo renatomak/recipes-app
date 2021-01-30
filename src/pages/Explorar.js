e3a47632fff767eb7e68a644a8e7cce43288ae66
renatomak <renato.mark.silva@gmail.com>
2021-01-30 15:14:24 -0300

    Requisito 69 - açoes dos botoes

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import Header from './components/Header';
 
-function Explorar() {
+function Explorar(props) {
+  const { history } = props;
   return (
     <div>
       <Header headerText="Explorar" showSearchButton="false" />
       <button
         type="button"
         data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
       >
         Explorar Comidas
       </button>
       <button
         type="button"
         data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
       >
         Explorar Bebidas
       </button>
     </div>
   );
 }
+
+Explorar.propTypes = {
+  history: ReactRouterPropTypes.history.isRequired,
+};
 
 export default Explorar;
