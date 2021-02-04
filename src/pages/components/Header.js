9c8875fb98e1399161eb6186a92660db86c81e4d
renatomak <renato.mark.silva@gmail.com>
2021-02-04 01:36:14 -0300

    Header

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import HeaderSearchBar from './HeaderSearchBar';
+import '../../css/header.css';
 
 function Header(props) {
   const { headerText, showSearchButton } = props;
   const [searchBar, setSearchBar] = useState(false);
   return (
-    <div>
+    <div className="header-content">
       <a
         type="button"
         data-testid="profile-top-btn"
@@ -18,19 +19,22 @@ function Header(props) {
         <img src={ profileIcon } alt="Profile icon" />
       </a>
       <h1 data-testid="page-title">{ headerText }</h1>
-      {JSON.parse(showSearchButton) && (
-        <button
-          type="button"
-          data-testid="search-top-btn"
-          src={ searchIcon }
-          onClick={ () => setSearchBar(!searchBar) }
-        >
-          <img src={ searchIcon } alt="Search icon" />
-        </button>
-      )}
-      {searchBar && (
-        <HeaderSearchBar headerText={ headerText } />
-      )}
+      <div className="item-search">
+        {searchBar && (
+          <HeaderSearchBar headerText={ headerText } />
+        )}
+        {JSON.parse(showSearchButton) && (
+          <button
+            className="search-top-btn"
+            type="button"
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            onClick={ () => setSearchBar(!searchBar) }
+          >
+            <img src={ searchIcon } alt="Search icon" />
+          </button>
+        )}
+      </div>
     </div>
   );
 }
