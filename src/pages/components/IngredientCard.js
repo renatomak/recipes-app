90374a43139255fae9cb859fbee0bb9ba72ee8bc
samantabelow <samanta.below@gmail.com>
2021-02-05 16:55:43 -0300

    Lint

--- src/pages/components/IngredientCard.js
+++ src/pages/components/IngredientCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
-/* import PropTypes from 'prop-types'; */
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 function IngredientCard(props) {
   const { ingredients, index, recipeType } = props;
@@ -16,14 +15,44 @@ function IngredientCard(props) {
     cardName = ingredients[index].strIngredient1;
   }
 
+  /*   const handleClick = async (ingredient) => {
+    let data = [];
+    if (recipeType === 'Comidas') {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+      data = response.json();
+    }
+    if (recipeType === 'Bebidas') {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
+      data = response.json();
+    }
+  }; */
+
   return (
-/*     <Link to={ `/${type}/${id}` }> */
-      <div data-testid={ `${index}-ingredient-card` }>
-        <img data-testid={ `${index}-card-img` } src={ cardImage } alt="card" />
-        <p data-testid={ `${index}-card-name` }>{ cardName }</p>
-      </div>
-/*     </Link> */
+    <div
+      data-testid={ `${index}-ingredient-card` }
+      /*       onClick={ () => { handleClick(ingredients[index]); } } */
+    >
+      <img data-testid={ `${index}-card-img` } src={ cardImage } alt="card" />
+      <p data-testid={ `${index}-card-name` }>{ cardName }</p>
+    </div>
   );
 }
 
 export default IngredientCard;
+
+IngredientCard.propTypes = {
+  recipeType: PropTypes.string.isRequired,
+  ingredients: PropTypes.arrayOf(PropTypes.oneOfType([
+    PropTypes.shape({
+      idIngredient: PropTypes.string,
+      strIngredient: PropTypes.string,
+      strDescription: PropTypes.string,
+    }),
+    PropTypes.shape({
+      strIngredient1: PropTypes.string,
+    }),
+  ])
+    .isRequired)
+    .isRequired,
+  index: PropTypes.number.isRequired,
+};
